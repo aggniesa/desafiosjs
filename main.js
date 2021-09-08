93382bb6945636fc34c25be801e9748ac9a479b5
Ines MS <inemenasaravia@gmail.com>
2021-09-08 20:21:13 -0300

    Desafío 2: Crear un algoritmo con un condicional

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-let numero1 = parseInt(prompt('Numero 1'));
+/*let numero1 = parseInt(prompt('Numero 1'));
 
 let numero2 = parseInt(prompt('Numero 2'));
 
-alert(numero1 + numero2);
+alert(numero1 + numero2);*/
+
+let numero = parseInt(prompt('Ingresá un número'));
+
+if (numero < 100 ) {
+    alert('El número es menor a 100');
+}
+else if  (numero == 100 ) {
+    alert ('El número es 100');
+}
+else {
+    alert('El número es mayor a 100');
+}
